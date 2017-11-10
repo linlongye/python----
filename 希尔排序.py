e2e1f9ea014369237b313f859add51d9810bff78
linlongye <linlongye@126.com>
2017-11-10 15:53:23 +0800

    完成希尔排序

--- 希尔排序.py
+++ 希尔排序.py
@@ -5,3 +5,19 @@
 def shell_sort(alist):
     n = len(alist)
     gap = n // 2
+    while gap >= 1:
+        for j in range(gap, n):
+            i = j
+            while i > 0:
+                if alist[i] < alist[i - gap]:
+                    alist[i], alist[i - gap] = alist[i - gap], alist[i]
+                    i -= gap
+                else:
+                    break
+        # 缩短gap步长
+        gap //= 2
+    return alist
+
+
+if __name__ == "__main__":
+    print(shell_sort([34, 32, 12, 56, 23, 98]))
